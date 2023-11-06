38f451c188866e2975812b85b5fa7f41d26e1a38
qw3ll <riddlealexandr@gmail.com>
2023-11-06 20:29:41 +0300

    Асинхронность данных о продукте Данные запроса в отдельном файле

--- async_product_details.py
+++ async_product_details.py
@@ -1,34 +1,40 @@
-import requests
+import aiohttp
+import asyncio
 from bs4 import BeautifulSoup
 import json
 
 
-def get_prod_data(params: str, cookies: str, headers: str) -> dict:
-    response = requests.post(
+async def get_product_details(params_list: list, cookies: str, headers: str) -> list:
+    async with aiohttp.ClientSession(cookies=cookies, headers=headers) as session:
+        fetch_tasks = [fetch_product_data(session, params) for params in params_list]
+        product_details_list = await asyncio.gather(*fetch_tasks)
+    return product_details_list
+
+async def fetch_product_data(session, params):
+    async with session.post(
         'https://www.dns-shop.ru/catalog/product/get-product-characteristics-actual/',
         params=params,
-        cookies=cookies,
-        headers=headers,
-    )
+    ) as response:
+        response_text = await response.text()
 
-    html_content = json.loads(response.text)["html"]
+    html_content = json.loads(response_text)["html"]
     soup = BeautifulSoup(html_content, 'html.parser')
 
-    groups = soup.find_all(class_="product-characteristics__group")
+    characteristic_groups = soup.find_all(class_="product-characteristics__group")
 
-    data = {}
+    product_data = {}
 
-    for group in groups:
+    for group in characteristic_groups:
         group_data = {}
-        spec_in_group = group.find_all(class_="product-characteristics__spec")
+        specs_in_group = group.find_all(class_="product-characteristics__spec")
         group_title = group.find(class_="product-characteristics__group-title").get_text(strip=True).replace(u'\xa0',
                                                                                                              u' ')
-        for spec in spec_in_group:
+        for spec in specs_in_group:
             spec_title = spec.find(class_="product-characteristics__spec-title").get_text(strip=True).replace(u'\xa0',
                                                                                                               u' ')
             spec_value = spec.find(class_="product-characteristics__spec-value").get_text(strip=True).replace(u'\xa0',
                                                                                                               u' ')
             group_data.update({spec_title: spec_value})
-        data.update({group_title: group_data})
+        product_data.update({group_title: group_data})
 
-    return data
+    return product_data
---
+++ request_data.py
@@ -0,0 +1,22 @@
+cookies = {
+    '_csrf': '3b5b377a269c13de8db6fe053835116dee3907d403125c6266ef0f533a5e90e3a%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_csrf%22%3Bi%3A1%3Bs%3A32%3A%22rEF2yn1xE8Q6OtSRLd9mOC06hL6z7X05%22%3B%7D',
+}
+
+headers = {
+    'Accept': '*/*',
+    'Accept-Language': 'ru,en;q=0.9,en-GB;q=0.8,en-US;q=0.7',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'DNT': '1',
+    'Origin': 'https://www.dns-shop.ru',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.76',
+    'X-CSRF-Token': 'DlyfQd_oGMWfwBi3ou7ldWsZ3HpVmytn06Xs2YxB9DR8GdlzpoYpvdr4SYHtmrYnJ33lFxrYG1G76dqjuxnEAQ==',
+    'X-Requested-With': 'XMLHttpRequest',
+    'content-type': 'application/x-www-form-urlencoded',
+    'sec-ch-ua': '"Chromium";v="118", "Microsoft Edge";v="118", "Not=A?Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+}
